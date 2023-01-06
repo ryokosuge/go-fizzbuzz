026177ec23be89b5208f717823834ac2c4a5d253
ryokosuge <ryo.kosuge@gmail.com>
2023-01-06 16:37:20 +0900

    refactor:	use switch

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,17 +3,14 @@ package fizzbuzz
 import "strconv"
 
 func Convert(n int) string {
-	if n % 5 == 0 && n % 3 == 0 {
+	switch {
+	case n % 15 == 0:
 		return "FizzBuzz"
-	}
-
-	if n % 5 == 0 {
+	case n % 5 == 0:
 		return "Buzz"
-	}
-
-	if n % 3 == 0 {
+	case n % 3 == 0:
 		return "Fizz"
+	default:
+		return strconv.Itoa(n)
 	}
-
-	return strconv.Itoa(n);
 }
