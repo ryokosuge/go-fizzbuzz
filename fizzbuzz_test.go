8aafe727dd25600bcf83d8d3ffe57094e0d8a552
ryokosuge <ryo.kosuge@gmail.com>
2023-01-06 16:28:57 +0900

    fix:	rename test name

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ryokosuge/go-fizzbuzz"
 )
 
-func TestFizz(t *testing.T) {
+func TestConvert(t *testing.T) {
 	got := fizzbuzz.Convert(1)
 	if got != "1" {
 		t.Errorf("Convert(1) is %q", got)
