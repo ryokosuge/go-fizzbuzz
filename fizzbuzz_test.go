5b83996a34b5ebce1597825f44d0fa6765145ec6
ryokosuge <ryo.kosuge@gmail.com>
2023-01-06 16:30:48 +0900

    test:	add pass 5 test failed

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -21,4 +21,9 @@ func TestConvert(t *testing.T) {
 	if got != "Fizz" {
 		t.Errorf("Convert(3) is %q", got)
 	}
+
+	got = fizzbuzz.Convert(5)
+	if got != "Buzz" {
+		t.Errorf("Convert(5) is %q", got)
+	}
 }
